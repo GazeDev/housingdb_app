75be81ee10efddac7708211d1275b58fbb3fd06a
zaclittleberry <zlittleberry@linuxfoundation.org>
2019-07-27 23:43:31 -0400

    update example config. copy it to config during build.

--- src/app/app.config.example.ts
+++ src/app/app.config.example.ts
@@ -1,12 +1,13 @@
 export const APP_CONFIG: any = {
-  apiURL: 'http://localhost:23085',
-  // apiURL: 'https://housingdb.surge.sh',
+  // apiURL: 'http://localhost:23085',
+  apiURL: 'https://gaze-housingdb.herokuapp.com',
 };
 
 import { KeycloakConfig } from 'keycloak-angular';
 
 export const KEYCLOAK_CONFIG: KeycloakConfig = {
-  url: 'https://accounts.gaze.dev/auth',
+  // url: 'https://accounts.gaze.dev/auth',
+  // url: 'http://localhost:8081/auth',
   realm: 'test-accounts',
   clientId: 'housingdb',
 };
