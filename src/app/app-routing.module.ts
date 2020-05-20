38ded97bdddd76fb581c38c0c4257909f64a96fb
zaclittleberry <zlittleberry@linuxfoundation.org>
2020-05-20 16:41:15 -0400

    revert routes array to non-programmatically generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,99 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-let pageModules = [
-  'home',
-  'dashboard',
-  'profile',
-  'properties',
-  'property-detail',
-  'property-edit',
-  'property-add',
-  'property-add-bulk',
-  'landlords',
-  'landlord-detail',
-  'landlord-edit',
-  'landlord-add',
-  'landlord-add-bulk',
-  'review-add',
-];
+// NOTE: we can't programmatically build the Routes array this way because the
+// AOT compiler isn't recognizing it. We can update/run this in a console to
+// generate the Routes array and then paste it in below.
+// let pageModules = [
+//   'home',
+//   'dashboard',
+//   'profile',
+//   'properties',
+//   'property-detail',
+//   'property-edit',
+//   'property-add',
+//   'property-add-bulk',
+//   'landlords',
+//   'landlord-detail',
+//   'landlord-edit',
+//   'landlord-add',
+//   'landlord-add-bulk',
+//   'review-add',
+// ];
+//
+// let pageRoutes = [];
+// for (let mod of pageModules) {
+//   let modClass = mod.split('-')
+//     .map(part => part[0].toUpperCase() + part.slice(1)).join('');
+//   pageRoutes.push({
+//     path: '',
+//     loadChildren: `_pages/${mod}/${mod}.module#${modClass}PageModule`
+//   });
+// }
 
-let pageRoutes: Routes = [];
-for (let mod of pageModules) {
-  let modClass = mod.split('-')
-    .map(part => part[0].toUpperCase() + part.slice(1)).join('');
-  pageRoutes.push({
-    path: '',
-    loadChildren: `_pages/${mod}/${mod}.module#${modClass}PageModule`
-  });
-}
+
+let routes: Routes = [
+  {
+    "path": "",
+    "loadChildren": "_pages/home/home.module#HomePageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/dashboard/dashboard.module#DashboardPageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/profile/profile.module#ProfilePageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/properties/properties.module#PropertiesPageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/property-detail/property-detail.module#PropertyDetailPageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/property-edit/property-edit.module#PropertyEditPageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/property-add/property-add.module#PropertyAddPageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/property-add-bulk/property-add-bulk.module#PropertyAddBulkPageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/landlords/landlords.module#LandlordsPageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/landlord-detail/landlord-detail.module#LandlordDetailPageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/landlord-edit/landlord-edit.module#LandlordEditPageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/landlord-add/landlord-add.module#LandlordAddPageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/landlord-add-bulk/landlord-add-bulk.module#LandlordAddBulkPageModule"
+  },
+  {
+    "path": "",
+    "loadChildren": "_pages/review-add/review-add.module#ReviewAddPageModule"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(
-    pageRoutes,
+    routes,
     {
       enableTracing: false, // <-- debugging purposes only
     }
