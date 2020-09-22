fb4ade8719e7fe50c8bf255a2c6106c61a2b1b49
Vita <wiebetc@gmail.com>
2020-09-22 15:51:47 -0700

    Fleshes out _models/housing-available.model.ts; \n Removes unnecessary code from housing-availables.page.ts;

---
+++ src/app/_models/housing-available.model.ts
@@ -0,0 +1,14 @@
+export class HousingAvailable {
+  id?: string; // guid
+  title: string;
+  body: string;
+  address: string;
+  website: string;
+  contact: string;
+  bedrooms?: number;
+  bathrooms?: number;
+  status: string;
+  AuthorId: string;
+  metadata: {};
+  details: {};
+}
--- src/app/_pages/housing-availables/housing-availables.page.ts
+++ src/app/_pages/housing-availables/housing-availables.page.ts
@@ -11,54 +11,23 @@ import { Property } from '_models/property.model';
 export class HousingAvailablesPage {
 
   loading: boolean = true;
-  properties: Property[];
+  housingAvailables: any;
   page: any;
-  landlords: any;
-  locations: any;
-  allLocations: any;
-
-  @ViewChild('ngFormDirective') formDirective;
-  form: FormGroup;
-  submitAttempt: boolean;
-  currentlySubmitting: boolean;
-
-  filtersOpen: boolean = false;
-  originalOrder: any;
 
   constructor(
-    private formBuilder: FormBuilder,
     private apiService: ApiService,
     private headService: HeadService,
   ) {
-    this.properties = [];
-    this.landlords = {};
-    this.locations = {};
-    this.allLocations = {};
-
+    this.housingAvailables = [];
     this.page = {
       offset: 0,
       size: 25,
     };
-
-    this.submitAttempt = false;
-    this.currentlySubmitting = false;
-    this.form = this.formBuilder.group({
-      name: [''],
-      address: [''],
-      bedrooms: [''],
-      locations: [''],
-    });
-
-    // used to preserve order in the keyvalue pipe in the template
-    this.originalOrder = (a: any, b: any): number => {
-      return 0;
-    }
   }
 
   ngOnInit() {
-    this.getProperties();
-    this.loadAllLocations();
-    this.headService.setPageTitle('Properties');
+    this.getHousingAvailables();
+    this.headService.setPageTitle('Housing Available: ');
   }
 
   ngOnDestroy() {
@@ -70,7 +39,7 @@ export class HousingAvailablesPage {
     this.page.size = $event.pageSize;
   }
 
-  getProperties(options = {}) {
+  getHousingAvailables(options = {}) {
     this.loading = true;
     this.apiService.getProperties(options).subscribe(res => {
       this.properties = res;
@@ -83,108 +52,4 @@ export class HousingAvailablesPage {
       this.loading = false;
     });
   }
-
-  extractAddress(property: any) {
-    let addr = property.PostalAddresses[0];
-    return `${addr.streetAddress}, ${addr.addressLocality}, ${addr.addressRegion} ${addr.postalCode}`;
-  }
-
-  extractNeighborhood(property: any) {
-    let addr = property.PostalAddresses[0];
-    return addr.addressNeighborhood;
-  }
-
-  loadPropertiesLandlords() {
-    for (let property of this.properties) {
-      if (property.LandlordId && !this.landlords.hasOwnProperty(property.LandlordId)) {
-        this.loadLandlord(property);
-      }
-    }
-  }
-
-  loadLandlord(property: any) {
-    // set a placeholder so we know not to load it again
-    this.landlords[property.LandlordId] = true;
-    this.apiService.getLandlord(property.LandlordId).subscribe(
-    res => {
-      // create a keyed array so we only have to load each landlord once
-      // and can access it in O(1).
-      this.landlords[property.LandlordId] = res;
-    },
-    err => {
-      console.log('Error loading Landlord', err);
-    });
-  }
-
-  loadPropertiesLocations() {
-    for (let property of this.properties) {
-      if (property.LocationId && !this.locations.hasOwnProperty(property.LocationId)) {
-        this.loadLocation(property);
-      }
-    }
-  }
-
-  loadLocation(property: any) {
-    // set a placeholder so we know not to load it again
-    this.locations[property.LocationId] = true;
-    this.apiService.getLocation(property.LocationId).subscribe(
-    res => {
-      // create a keyed array so we only have to load each landlord once
-      // and can access it in O(1).
-      this.locations[property.LocationId] = res;
-    },
-    err => {
-      console.log('Error loading location', err);
-    });
-  }
-
-  loadAllLocations() {
-    this.apiService.getLocations().subscribe(res => {
-      this.buildAllLocations(res);
-    });
-  }
-
-  buildAllLocations(locations) {
-    for (let state of locations) {
-      for (let city of state.children) {
-        if (city.children.length === 1 && city.name === city.children[0].name) {
-          this.allLocations[city.children[0].id] = {
-            name: city.children[0].name,
-          };
-        } else {
-          this.allLocations[city.id] = {
-            name: city.name,
-            children: {},
-          };
-          for (let location of city.children) {
-            this.allLocations[city.id].children[location.id] = {
-              name: location.name,
-            };
-          }
-        }
-      }
-    }
-  }
-
-  toggleFilters() {
-    if (this.filtersOpen === true) {
-      this.filtersOpen = false;
-    } else {
-      this.filtersOpen = true;
-    }
-  }
-
-  resetForm() {
-    this.form.reset();
-    this.formDirective.resetForm();
-    // if we're clearing the form, we're clearing the filters, so we re-submit
-    this.submit();
-  }
-
-  submit() {
-    this.loading = true;
-    this.getProperties(this.form.value);
-  }
-
-
 }
